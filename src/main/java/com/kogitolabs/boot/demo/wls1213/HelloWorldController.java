69ffb0a536d6d450fd77798b5a74e967c72eee89
Abdul Rahman Arraisi <itsarraisi@gmail.com>
2019-09-05 17:09:54 +0700

    add thymleaf

--- src/main/java/com/kogitolabs/boot/demo/wls1213/HelloWorldController.java
+++ src/main/java/com/kogitolabs/boot/demo/wls1213/HelloWorldController.java
@@ -1,15 +1,15 @@
 package com.kogitolabs.boot.demo.wls1213;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/resource")
+@Controller
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    String readResource() {
-        return "Hello World!!";
+    @RequestMapping(method = RequestMethod.GET, value = "/resource")
+    String readResource(Model model) {
+        return "index";
     }
 }
