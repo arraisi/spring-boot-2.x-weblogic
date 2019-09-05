86bfdcbaec7501934711a4c3b3bbe4d1c96a9e70
Abdul Rahman Arraisi <itsarraisi@gmail.com>
2019-09-05 16:59:23 +0700

    add pom ojdbc oracle

---
+++ src/main/java/com/kogitolabs/boot/demo/wls1213/config/JdbcConfiguration.java
@@ -0,0 +1,26 @@
+package com.kogitolabs.boot.demo.wls1213.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource(value = {"classpath:application.properties"})
+public class JdbcConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        dataSource.setUrl("jdbc:oracle:thin:@192.168.100.12:1522/xe");
+        dataSource.setUsername("auth");
+        dataSource.setPassword("auth");
+        return dataSource;
+    }
+
+}
